042f3a87a605e5f771df4cad5a6eff6d0df8a790
dhavalp95 <patoliyadhaval555@gmail.com>
2024-11-08 17:14:47 +0530

    Add SPM setup and create one example

---
+++ Sources/SevenILibraryExample/SevenILibraryExample.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+public class ColorChangingView: UIView {
+    private var tapGestureRecognizer: UITapGestureRecognizer!
+    private var colorToChangeTo: UIColor?
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+
+    private func initialize() {
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        addGestureRecognizer(tapGestureRecognizer)
+        isUserInteractionEnabled = true
+        backgroundColor = UIColor.random
+    }
+
+    public func setColorOnTap(to color: UIColor = .red) {
+        colorToChangeTo = color
+    }
+
+    @objc private func handleTap(_ sender: UITapGestureRecognizer) {
+        self.backgroundColor = UIColor.random
+        
+    }
+}
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1.0
+        )
+    }
+}
